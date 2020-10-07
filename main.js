14db4791ab4bc25f936911e233b79f1a8727b7e8
Gregory.Johnson <3531op@gmail.com>
2020-10-07 10:26:32 -0400

    intitial commit

--- main.js
+++ main.js
@@ -1,16 +1,112 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+// * `xify` - returns the same string, 
+// but with every character replaced by an 'x'
+function xify(str) {
+  let newStr = '';
+  for (let i = 0; i < str.length; i++) {
+    newStr = newStr + 'x'
+  }
+  return newStr
+}
+
+// * `smilify` - returns the same string, but with every 
+// character replaced by a smiley face
+function smilify(str) {
+  let newStr = '';
+  for (let i = 0; i < str.length; i++) {
+    newStr = newStr + '😊'
+  }
+  return newStr
+}
+
+// * `yellingChars` - returns the given string with an 
+// exclamation point after each character
+function yellingChars(str) {
+  let newStr = ''
+  for (let i = 0; i < str.length; i++) {
+    newStr = newStr + str[i] + '!'
+  }
+  return newStr
+}
+
+function indexedChars(str) {
+  let newStr = '';
+  for (let i = 0; i < str.length; i++) {
+    newStr = newStr + [i] + str[i]
+  }
+  return newStr
+}
+
+function exclaim(str) {
+  let newStr = '';
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '?' || str[i] === '.') {
+      newStr = newStr + '!'
+
+    } else {
+      newStr = newStr + str[i]
+    }
+  }
+  return newStr
+}
+
+function repeatIt(str, count) {
+  let newStr = '';
+  for (let i = 0; i < str.length; i++) {
+    newStr = str.repeat(count)
+  }
+  return newStr
+}
+
+function truncate(str) {
+  let newStr = '';
+  for (let i = 0; i < str.length; i++) {
+    if (str.length > 18) {
+      newStr = str.slice(0, 15) + '...'
+
+    } else {
+      newStr = str
+    }
+    return newStr
+  }
+}
+
+function ciEmailify(name) {
+  let result = '';
+  for (let i = 0; i < name.length; i++) {
+    if (name[i] === ' ') {
+      result = result + '.';
+  } else {
+    result = result + name[i].toLowerCase();
+  }
+}
+return result + '@codeimmersives.com'
+}
+
+function reverse(str) {
+  let newStr = ''
+  for (let i = 0; i < str.length; i++) {
+    newStr = str[i] + newStr
+  }
+  return newStr
+}
+
+function onlyVowels(str) {
+  let newStr = '';
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u')  {
+      newStr = newStr 
+    }
+  return newStr + str[i]
+  }
+
+}
 
 
 
-
-
-
-
-
-
- /********************************************
+/********************************************
  * CODE DOWN HERE IS FOR INTERNAL USE ONLY. *
  *           PLEASE DON'T TOUCH!            *
  ********************************************/
@@ -22,6 +118,11 @@ if (typeof xify === 'undefined') {
 if (typeof smilify === 'undefined') {
   smilify = undefined;
 }
+
+if (typeof yellingChars === 'undefined') {
+  yellingsChars = undefined;
+}
+
 
 if (typeof numberedChars === 'undefined') {
   numberedChars = undefined;
@@ -75,6 +176,7 @@ if (typeof crazyCase2ReturnOfCrazyCase === 'undefined') {
 module.exports = {
   xify,
   smilify,
+  yellingChars,
   indexedChars,
   numberedChars,
   exclaim,
